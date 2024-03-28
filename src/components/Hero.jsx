4ec81c5d713ebafcc1112b53edd6cf6d2065bd2f
gdong6 <gxmacbookpro@GX-MacBook-Pro.local>
2024-03-28 14:01:47 +0800

    Hero All OK

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,12 @@ import { grandpa } from '../assets'
 const Hero = () => {
   return (
     <div className='flex w-full h-screen  justify-center'>
-      <div className="flex sm:px-0 max-w-7xl flex-col lg:flex-row justify-end gap-12">
-        <div className='flex-col content-center'>
-            <h1 className='text-white font-sans lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2'>
+      <div className="flex sm:px-0 max-w-7xl flex-col lg:flex-row justify-end">
+        <div className='flex-col m-auto '>
+            <h1 className='text-white font-sans lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[50px] lg:leading-[98px] mt-2'>
               {detail.name}
             </h1>
-            <p className='text-white font-sans lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]'> 
+            <p className='text-white font-sans lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[20px] lg:leading-[40px]'> 
               🕯 {detail.birth} - {detail.death} 
             </p>
         </div>
